dfa7b4be14758cd62ddb739f1e4fde07e32a1ee9
levenx <1025534801@qq.com>
2021-11-28 14:45:11 +0800

    feat: table;

--- packages/dumbooo/src/components/Table/index.stories.tsx
+++ packages/dumbooo/src/components/Table/index.stories.tsx
@@ -1,37 +1,88 @@
 import React, { FunctionComponent, ComponentProps } from 'react';
-import Button from './index';
+import Table from './index';
 
 // control参考文档： https://xiday.com/2020/09/27/storybook/
 export default {
-    component: Button,
-    title: 'Button',
-    argTypes: {
-        block: {
-            control: {
-                type: 'boolean',
-            },
-        },
-        type: {
-            control: {
-                type: 'inline-radio',
-                options: ['default', 'primary', 'danger', 'dashed', 'text', 'link']
-            }
-        },
-        shape: {
-            control: {
-                type: 'inline-radio',
-                options: ['default', 'circle', 'round'],
-                default: 'default'
-            }
-        },
-    }
+    component: Table,
+    title: 'Table'
 };
 
-export const Default: FunctionComponent<ComponentProps<typeof Button>> = ({ ...props }) => {
+export const Default: FunctionComponent<ComponentProps<typeof Table>> = ({ ...props }) => {
     console.log('--->', props)
+    const columns = [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+          render: text => <a>{text}</a>,
+        },
+        {
+          title: 'Age',
+          dataIndex: 'age',
+          key: 'age',
+        },
+        {
+          title: 'Address',
+          dataIndex: 'address',
+          key: 'address',
+        },
+        {
+            title: 'Tags',
+            key: 'tags',
+            dataIndex: 'tags',
+            render: tags => (
+              <>
+                {tags.map(tag => {
+                  let color = tag.length > 5 ? 'geekblue' : 'green';
+                  if (tag === 'loser') {
+                    color = 'volcano';
+                  }
+                  return (
+                    <div color={color} key={tag}>
+                      {tag.toUpperCase()}
+                    </div>
+                  );
+                })}
+              </>
+            ),
+          },
+          {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+              <div>
+                <a>Invite {record.name}</a>
+                <a>Delete</a>
+              </div>
+            ),
+          },
+        ];
+
+        const data = [
+            {
+              key: '1',
+              name: 'John Brown',
+              age: 32,
+              address: 'New York No. 1 Lake Park',
+              tags: ['nice', 'developer'],
+            },
+            {
+              key: '2',
+              name: 'Jim Green',
+              age: 42,
+              address: 'London No. 1 Lake Park',
+              tags: ['loser'],
+            },
+            {
+              key: '3',
+              name: 'Joe Black',
+              age: 32,
+              address: 'Sidney No. 1 Lake Park',
+              tags: ['cool', 'teacher'],
+            },
+          ];
+
     return <div>
-        <Button onClick={() => { console.log('click!!') }} {...props}>
-            测试
-        </Button>
+        <Table dataSource={data} columns={columns} rowKey="key" />
     </div>
 }
--- packages/dumbooo/src/components/Table/index.tsx
+++ packages/dumbooo/src/components/Table/index.tsx
@@ -3,29 +3,46 @@ import classnames from 'classnames';
 import { BaseType } from '../typing';
 import './index.less';
 
-export interface IButton extends BaseType {
-    block?: boolean;
-    size?: 'large' | 'middle' | 'small';
-    type?: 'default' | 'primary' | 'danger' | 'dashed' | 'text' | 'link';
-    shape?: 'default' | 'circle' | 'round';
+export interface ITable {
+    /**
+     * 是否展示外边框和列边框
+     * @default true
+     */
+    bordered?: boolean;
+    /**
+     * 自定义行类名
+     */
+    rowClassName?: string;
+    columns: any;
+    dataSource: any[];
+    rowKey: string | Function;
     loading?: boolean;
-    disabled?: boolean;
+    rowSelection?: object;
+    scroll?: object;
+    showHeader?: boolean;
 }
 
-export default function Button(props: IButton) {
-    const { type = "default", block, shape, disabled, children, onClick, } = props;
+export default function Table(props: ITable) {
+    const { showHeader = true, bordered = true, dataSource, rowKey, columns } = props;
+    console.log(columns, dataSource)
     return (
-        <div>
-            <button
-                disabled={disabled}
-                className={classnames("dumbo-button", `dumbo-button--${type}`,
-                    {
-                        'dumbo-button--block': block,
-                        'dumbo-button--circle': shape === 'circle',
-                        'dumbo-button--disabled': disabled
-                    })} onClick={onClick}>
-                {children}
-            </button>
+        <div className="dumbo-table-content">
+            <table className="dumbo-table">
+                {showHeader && <thead className="dumbo-table-thead">
+                    <tr>
+                        {columns.map((item) => <th className="dumbo-table-cell">{item.title}</th>)}
+                    </tr>
+                </thead>}
+                <tbody className="dumbo-table-tbody">
+                    {dataSource.map((rowitem: any) => {
+                        return (
+                            <tr>
+                                {columns.map((item: any) => (<td key={item.dataIndex} className="dumbo-table-cell">{item.render?.(rowitem[item.key], rowitem) || rowitem[item.key]}</td>))}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     )
 }
